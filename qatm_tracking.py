9bebebc90c756fd534994af262831b6002cbed79
setarekhosravi <strhkhosravi@gmail.com>
2025-04-07 16:00:37 +0330

    update code, it can track objects but it is not stable, cuda may be out of memory

--- qatm_tracking.py
+++ qatm_tracking.py
@@ -48,7 +48,7 @@ def create_template(frame, detection):
     """
     x1, y1, x2, y2 = detection
     template = frame[y1:y2, x1:x2]
-    cv2.write("template/template.png", template)
+    cv2.imwrite("template/template.png", template)
     return template
 
 def tracking(args):
@@ -61,17 +61,28 @@ def tracking(args):
     model = CreateModel(model=models.vgg19(pretrained=True).features, alpha=args.alpha, use_cuda=args.cuda)
     
     print('One Sample Image Is Inputted')
-    image_path = "sample/"
+    image_path = "sample/sample.png"
     dataset = ImageDataset(Path(template_dir), image_path, thresh_csv='thresh_template.csv')
     print("calculate score...")
     scores, w_array, h_array, thresh_list = run_multi_sample(model, dataset)
     print("nms...")
     boxes, indices = nms_multi(scores, w_array, h_array, thresh_list)
     d_img = plot_result_multi(dataset.image_raw, boxes, indices, show=False, save_name='result.png')
+    del(dataset)
     return d_img, boxes #[x1, y1, x2, y2]
+
+def yolo_detection(args, frame):
+    model = torch.hub.load('ultralytics/yolov5', 'custom', path= args.weights, force_reload= False)
+    model.float()
+    model.eval()    
+    results = model(frame)
+    bbox = results.xyxy[0].cpu().numpy()
+    return bbox
 
 def args_parser():
     parser = argparse.ArgumentParser(description='QATM Pytorch Implementation')
+    parser.add_argument('--video_path', type=str, default='video.mp4')
+    parser.add_argument('--weights', type= str, default='weights/yolov5s.pt')
     parser.add_argument('--cuda', action='store_true')
     parser.add_argument('-r', '--result_images_dir', default='result/')
     parser.add_argument('--alpha', type=float, default=25)
@@ -82,4 +93,35 @@ def args_parser():
 
 if __name__ == '__main__':
     args = args_parser()
-    pass
+    video_path = args.video_path
+    cap = cv2.VideoCapture(video_path)
+    frame_number = 0
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+        convert_video_to_frames(frame)
+        if frame_number==0:
+            bbox_det = yolo_detection(args, frame)
+            x1,y1,x2,y2,_,_ = bbox_det[0]
+        else:
+            create_template(frame, [int(x1),int(y1),int(x2),int(y2)])
+            d_img, bbox_tr = tracking(args)
+            bbox_coords = bbox_tr[0]  # Get the first (and only) element
+            x1, y1 = bbox_coords[0]   # First point (top-left)
+            x2, y2 = bbox_coords[1]
+        
+        if frame_number == 0:
+            annotated_frame = frame.copy()
+            cv2.rectangle(annotated_frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 2)
+            cv2.imshow('frame', annotated_frame)
+        else:
+            cv2.imshow('frame', d_img)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        if frame_number!=0:
+            del(d_img)
+            torch.cuda.empty_cache()
+        frame_number +=1
+    cap.release()
+    cv2.destroyAllWindows()
